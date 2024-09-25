b626d9cf43d6bd05e879521fa0caf3355681bac5
Asmaa26189 <engasmaahesham@gmail.com>
2024-09-25 15:41:44 +0300

    clean

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -1,7 +1,6 @@
 // this is a logger to use middleware
 import { Request, Response, NextFunction } from 'express';
 const logger = (req: Request, res: Response, next: NextFunction): void => {
-  console.log('Middleware Start');
   next();
 };
 export default logger;
